fafd4dec247f5d34f813d687522873021a1aac3f
katarina.mosic <kata.99@hotmail.rs>
2022-03-02 00:09:26 +0100

    Mini projekat

---
+++ sandbox/Katarina Mosic/projekat/PaymentApp/src/app/shared/payment-detail.model.ts
@@ -0,0 +1,12 @@
+export class PaymentDetail {
+    paymentDetailId:number=0;
+    cardOwnerName:string="";
+    cardNumber:string="";
+    expirationDate:string="";
+    securutyCode:string="";
+
+    constructor(){
+         this.paymentDetailId = 0;
+         //this.cardNumber
+    }
+}
