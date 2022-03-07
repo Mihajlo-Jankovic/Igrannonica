72aa2f332a15e29be40898b0192accf134f8430d
Milan Stosic <milanstosic00@gmail.com>
2022-03-07 20:33:56 +0100

    HttpClient i HttpRequest
    Slanje jednostavnih get zahteva iz C# i vracanje json objekta sa python servera

---
+++ sandbox/Milan Stosic/KonekcijaSaPythonom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace KonekcijaSaPythonom
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.DoSomething();
+        }
+        private async Task DoSomething()
+        {
+            string response = await client.GetStringAsync("http://localhost:8080");
+            Console.WriteLine(response);
+            Console.ReadKey();
+        }
+    }
+}
