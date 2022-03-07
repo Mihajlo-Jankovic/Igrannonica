4b53511efaa1147e2ad669f0fccd101232467bcf
Jelena Tomic <j.tomic3273@gmail.com>
2022-03-07 10:56:25 +0100

    Model User
    Dodat model User

---
+++ back/Igrannonica/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Igrannonica.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName ="nvarchar(50)")]
+        public string firstname { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string lastname { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string email { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string password { get; set; }
+    }
+}
