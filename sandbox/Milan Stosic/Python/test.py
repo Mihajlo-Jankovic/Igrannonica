3fcbde5126d3747ca63f4c64ad274fe83073f2e1
Milan Stosic <milanstosic00@gmail.com>
2022-03-07 21:28:20 +0100

    Python server

---
+++ sandbox/Milan Stosic/Python/test.py
@@ -0,0 +1,26 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import cgi
+import json
+
+class requestHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}).encode()) 
+
+def main():
+    PORT = 8080
+    server_address = ('localhost', PORT)
+    server = HTTPServer(server_address, requestHandler)
+    print('Server running on port %s' % PORT)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
