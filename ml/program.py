b34e7361e57c14f7e7c449a0b1e27d17e6bd2d48
Mihajlo Jankovic <mihajlojankovic44@gmail.com>
2022-03-08 23:59:21 +0100

    Dodata metoda za izracunavanje statistickih podataka

--- ml/program.py
+++ ml/program.py
@@ -1,14 +1,13 @@
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 import csv
 
 #tf.enable_eager_execution()
 
-path = 'csv\movies.csv'
-
 #defaults = [tf.int32] * 55
 #dataset = tf.contrib.data.CsvDataset(path, defaults)
 
+'''
 tmp = [21,22,-108,31,-1,32,34,31]
 dataset = tf.data.Dataset.from_tensor_slices(tmp)
 
@@ -19,6 +18,8 @@ print(dataset)
 
 for i in dataset:
     print(i)
+'''
+
 
 '''
 with open(path,'r') as infile:
@@ -29,9 +30,36 @@ with open(path,'r') as infile:
         print(row[0])
 '''
 
-df = pd.read_csv(path, index_col=0)
+# Izracunavanje statistika za odredjenu kolonu iz tabele
+def statistics(df,colIndex):
+    col = df.columns[colIndex]
 
-print()
-print()
-print()
-print(df)
+    rowsNum = df.shape[0] # Ukupan broj podataka za kolonu
+    min = df[col].min() # Minimum
+    max = df[col].max() # Maksimum
+    avg = df[col].mean() # Srednja vrednost
+    med = df[col].median() # Mediana
+    firstQ, thirdQ = df[col].quantile([.25, .75]) # Prvi i treci kvartil
+    corrMatrix = df.corr() # Korelaciona matrica
+
+    return (rowsNum,min,max,avg,med,firstQ,thirdQ,corrMatrix)
+
+path = 'csv\movies.csv'
+
+with open(path) as f: 
+    header = csv.Sniffer().has_header(f.read(1024)) # Proverava da li u fajlu postoji header
+
+if(header): 
+    df = pd.read_csv(path, index_col = 0) 
+
+    df.columns = [col.lower() for col in df]
+    df.columns = [col.strip('-$%') for col in df]
+    df.columns = [col.strip() for col in df]
+    df.columns = [col.replace(' ','_') for col in df]
+
+else: 
+    df = pd.read_csv(path, header = None) 
+
+df.dropna()
+
+stats = statistics(df,8)
