d1671f5d1291b1b95c6356d114bc1b64c55d7231
katarina.mosic <kata.99@hotmail.rs>
2022-03-10 14:36:34 +0100

    funkcija register - izmena

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { UserService } from 'src/app/services/user.service';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -11,65 +11,71 @@ import { HttpClient} from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-/*
-  username: any;
-  password: any;
-  invalidLogin: boolean;
-  //constructor(public service: UserService, private cookieService: CookieService, private router:Router) { }
-  constructor(private router: Router, private http: HttpClient) {}
-
-  ngOnInit(): void {
-  }
-
-  /*onLogin1()
-  {
-    if(this.username && this.password) 
-    {
-        this.service.login(this.username, this.password).subscribe((token) => {
-        if (token) {
-          this.cookieService.set("token", token);
-          this.router.navigate(["/home"]);
-        }
-        else {
-          alert("Greska");
-        }
-      }
-      )
-    }
-  }
-
-  onLogin(form: NgForm) {
-    const credentials = {
-        'username' : form.value.username,
-        'password' : form.value.password
-    }
-
-    this.http.post("https://localhost:7219/api/User/login", credentials)
-        .subscribe(response => {
-            const token = (<any>response).token;
-            localStorage.setItem("jwt", token);
-            this.invalidLogin = false;
-            this.router.navigate(["/"]);
-        }, err => {
-            this.invalidLogin = true;
-        })
-    }
-
-    */
-
-  username : string;
-  password : string;
-  constructor(private library : UserService, private cookie : CookieService, private router : Router) { }
-
-  ngOnInit(): void {
+  /*
+    username: any;
+    password: any;
+    invalidLogin: boolean;
+    //constructor(public service: UserService, private cookieService: CookieService, private router:Router) { }
+    constructor(private router: Router, private http: HttpClient) {}
   
+    ngOnInit(): void {
+    }
+  
+    /*onLogin1()
+    {
+      if(this.username && this.password) 
+      {
+          this.service.login(this.username, this.password).subscribe((token) => {
+          if (token) {
+            this.cookieService.set("token", token);
+            this.router.navigate(["/home"]);
+          }
+          else {
+            alert("Greska");
+          }
+        }
+        )
+      }
+    }
+  
+    onLogin(form: NgForm) {
+      const credentials = {
+          'username' : form.value.username,
+          'password' : form.value.password
+      }
+  
+      this.http.post("https://localhost:7219/api/User/login", credentials)
+          .subscribe(response => {
+              const token = (<any>response).token;
+              localStorage.setItem("jwt", token);
+              this.invalidLogin = false;
+              this.router.navigate(["/"]);
+          }, err => {
+              this.invalidLogin = true;
+          })
+      }
+  
+      */
+
+  username: string;
+  password: string;
+  constructor(private library: UserService, private cookie: CookieService, private router: Router) { }
+
+  ngOnInit(): void {
+
   }
 
   login() {
-    this.library.login(this.username, this.password).subscribe(token => {
-      this.cookie.set("token", token);
-      this.router.navigate(['/home']);
-    })
+    if (this.username && this.password) {
+      this.library.login(this.username, this.password).subscribe(token => {
+        this.cookie.set("token", token);
+        this.router.navigate(['/home']);
+      })
+    }
+    else
+    {
+      alert("Pogresno korisnicko ime ili lozinka.");
+    }
   }
 }
 
