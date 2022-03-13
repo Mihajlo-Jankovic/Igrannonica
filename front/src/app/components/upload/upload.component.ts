1cabc6aaff142afaac9810f47307273dcd5dc901
Jelena Tomic <j.tomic3273@gmail.com>
2022-03-13 21:25:23 +0100

    Upload CSV fajla - front
    Omoguceno upload-ovanje CSV fajla

--- front/src/app/components/upload/upload.component.ts
+++ front/src/app/components/upload/upload.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
   }
+
+public uploadFile(files : any)
+{
+  if(files.length === 0)
+    return;
+  
+  let file = <File>files[0];
+  var fileSize = file.size;
+  console.log(fileSize);
+  if(fileSize / 1048576 > 500)
+    alert("Maximum file size is 500MB");
+  else{
+  const formData = new FormData();
+  formData.append('file', file, file.name);
+
+  this.http.post('https://localhost:7219/', formData).subscribe(err =>
+  {
+    if(err)
+    {
+      console.log(err);
+    }
+  })
+}
+}
 
 }
