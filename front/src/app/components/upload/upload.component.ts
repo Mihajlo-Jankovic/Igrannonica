5864281dd570ec17dfab8f9d468b3f326d3c85d6
katarina.mosic <kata.99@hotmail.rs>
2022-03-15 19:04:47 +0100

    Upload strana - dopuna

--- front/src/app/components/upload/upload.component.ts
+++ front/src/app/components/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-upload',
@@ -8,33 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor(private http : HttpClient) { }
+  loggedUser: boolean;
+
+  constructor(private http : HttpClient, private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.loggedUser = this.loginService.isAuthenticated();
+    console.log(this.loggedUser);
+    /*this.files[0] = "a";
+    this.files[1] = "b";
+    this.files[2] = "c";
+    */
   }
 
-public uploadFile(files : any)
-{
-  if(files.length === 0)
-    return;
-  
-  let file = <File>files[0];
-  var fileSize = file.size;
-  console.log(fileSize);
-  if(fileSize / 1048576 > 500)
-    alert("Maximum file size is 500MB");
-  else{
-  const formData = new FormData();
-  formData.append('file', file, file.name);
-
-  this.http.post('https://localhost:7219/api/Upload', formData).subscribe(err =>
+  public uploadFile(files : any)
   {
-    if(err)
+    if(files.length === 0)
+      return;
+    
+    let file = <File>files[0];
+    var fileSize = file.size;
+    console.log(fileSize);
+    if(fileSize / 1048576 > 500)
+      alert("Maximum file size is 500MB");
+    else{
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+
+    this.http.post('https://localhost:7219/api/Upload', formData).subscribe(err =>
     {
-      console.log(err);
-    }
-  })
-}
-}
+      if(err)
+      {
+        console.log(err);
+      }
+    })
+  }
+  }
 
 }
