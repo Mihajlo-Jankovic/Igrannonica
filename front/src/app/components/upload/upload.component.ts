a84f923944473b51c889ac86ecf886b2a8f4e264
Ana <apetrovicpmf@gmail.com>
2022-03-15 19:05:26 +0100

    Tabela za prikazivanje podataka iz csv fajla

--- front/src/app/components/upload/upload.component.ts
+++ front/src/app/components/upload/upload.component.ts
@@ -12,6 +12,9 @@ export class UploadComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  headingLines: any = [];
+  rowLines: any = [];
+
 
 public uploadFile(files : any)
 {
@@ -24,17 +27,54 @@ public uploadFile(files : any)
   if(fileSize / 1048576 > 500)
     alert("Maximum file size is 500MB");
   else{
-  const formData = new FormData();
-  formData.append('file', file, file.name);
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+////////////////////////////////////
 
-  this.http.post('https://localhost:7219/api/Upload', formData).subscribe(err =>
-  {
-    if(err)
+let reader: FileReader =  new FileReader();
+            reader.readAsText(file);
+            reader.onload = (e) => {
+                let csv: any = reader.result;
+                let allTextLines = [];
+                allTextLines = csv.split('\n');
+                
+                let headers = allTextLines[0].split(/;|,/);
+                let data = headers;
+                let headersArray = [];
+
+                for (let i = 0; i < headers.length; i++) {
+                    headersArray.push(data[i]);
+                }
+                this.headingLines.push(headersArray);
+
+                let rowsArray = [];
+
+                let length = allTextLines.length - 1;
+                
+                let rows = [];
+                for (let i = 1; i < length; i++) {
+                    rows.push(allTextLines[i].split(/;|,/));
+                }
+                length = rows.length;
+                for (let j = 0; j < length; j++) {
+                    rowsArray.push(rows[j]);
+                }
+                this.rowLines.push(rowsArray);
+                console.log(this.rowLines);
+            }
+/////////////////////////////////////
+    this.http.post('https://localhost:7219/api/Upload', formData).subscribe(err =>
     {
-      console.log(err);
-    }
-  })
-}
+      if(err)
+      {
+        console.log(err);
+      }
+    })
+  }
+
+
+
+
 }
 
 }
