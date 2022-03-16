ce990478caa670cd2c8432413310d759b005776b
Ana <apetrovicpmf@gmail.com>
2022-03-16 20:22:16 +0100

    Izmena koda (obrisan deo koda)

--- front/src/app/components/upload/upload.component.ts
+++ front/src/app/components/upload/upload.component.ts
@@ -44,38 +44,6 @@ public uploadFile(files : any)
     const formData = new FormData();
     formData.append('file', file, file.name);
 
-    let reader: FileReader =  new FileReader();
-                reader.readAsText(file);
-                reader.onload = (e) => {
-                    let csv: any = reader.result;
-                    let allTextLines = [];
-                    allTextLines = csv.split('\n');
-                    
-                    let headers = allTextLines[0].split(/;|,/);
-                    let data = headers;
-                    let headersArray = [];
-
-                    for (let i = 0; i < headers.length; i++) {
-                        headersArray.push(data[i]);
-                    }
-                    this.headingLines.push(headersArray);
-
-                    let rowsArray = [];
-
-                    let length = allTextLines.length - 1;
-                    
-                    let rows = [];
-                    for (let i = 1; i < length; i++) {
-                        rows.push(allTextLines[i].split(/;|,/));
-                    }
-                    length = rows.length;
-                    for (let j = 0; j < length; j++) {
-                        rowsArray.push(rows[j]);
-                    }
-                    this.rowLines.push(rowsArray);
-                    console.log(this.rowLines);
-                }
-
     this.http.post('https://localhost:7219/api/Upload', formData).subscribe(err =>
     {
       if(err)
