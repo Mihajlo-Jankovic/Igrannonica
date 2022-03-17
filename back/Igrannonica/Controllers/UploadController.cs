b86b4c19931773f3f23837576aa77b381e1bc4fa
andrija.mitrovic <andrijamitrovic11@gmail.com>
2022-03-17 12:18:46 +0100

    popravljen UploadController

--- back/Igrannonica/Controllers/UploadController.cs
+++ back/Igrannonica/Controllers/UploadController.cs
@@ -1,9 +1,11 @@
 ﻿using Igrannonica.Models;
+using LumenWorks.Framework.IO.Csv;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Sylvan.Data.Csv;
+using System.Data;
 using System.Net.Http.Headers;
 
 namespace Igrannonica.Controllers
@@ -12,7 +14,7 @@ namespace Igrannonica.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
-        [HttpPost,DisableRequestSizeLimit]
+        [HttpPost, DisableRequestSizeLimit]
         public async Task<IActionResult> Upload()
         {
             try
@@ -20,60 +22,47 @@ namespace Igrannonica.Controllers
                 CSVFile DBFile = new CSVFile();
                 var client = new MongoClient("mongodb://localhost:27017");
                 var database = client.GetDatabase("IgrannonicaDB");
-                var collection = database.GetCollection<CSVFile>("CSVFiles");
                 var file = Request.Form.Files[0];
 
-                if(file.Length > 0)
+                if (file.Length > 0)
                 {
-                    using var fileStream = file.OpenReadStream();
-                    var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(fileStream);
-                    parser.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited;
-                    parser.SetDelimiters(new string[] { ";" });
-                    var fileContents = parser.ReadToEnd();
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    DBFile.FileName = fileName;
-                    DBFile.File = fileContents;
-                    DBFile.User_ID = 1;
-                    await collection.InsertOneAsync(DBFile);
+                    var fileStream = file.OpenReadStream();
+                    DataTable csvTable = new DataTable();
+                    using (CsvReader csvReader =
+                        new CsvReader(new StreamReader(fileStream), true))
+                    {
+                        csvTable.Load(csvReader);
+                    }
+
+                    await SaveDataTableToCollection(csvTable, fileName);
 
                     return Ok("okej");
                 }
-                return BadRequest( "nema fajla" );
+                return BadRequest("nema fajla");
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
-        [HttpPost, DisableRequestSizeLimit]
-        public IActionResult Upload()
+            }
+        }
+
+        [NonAction]
+        public async Task SaveDataTableToCollection(DataTable dt, string fileName)
         {
-            try
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("IgrannonicaDB");
+            database.CreateCollection(fileName);
+            var collection = database.GetCollection<BsonDocument>(fileName);
+
+            List<BsonDocument> batch = new List<BsonDocument>();
+            foreach (DataRow dr in dt.Rows)
             {
-                var file = Request.Form.Files[0];
-                var folderName = Path.Combine("Resources", "CSVFiles");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-
-                if(file.Length > 0)
-                {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-
-                    using(var stream = new FileStream(fullPath,FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-
-                    return Ok(new { dbPath });
-                }
-                else
-                {
-                    return BadRequest("file not found.");
-                }
+                var dictionary = dr.Table.Columns.Cast<DataColumn>().ToDictionary(col => col.ColumnName, col => dr[col.ColumnName]);
+                batch.Add(new BsonDocument(dictionary));
             }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
+
+            await collection.InsertManyAsync(batch.AsEnumerable());
         }
     }
 }
