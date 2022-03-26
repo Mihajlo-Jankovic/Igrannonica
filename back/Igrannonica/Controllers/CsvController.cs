94124dbd480ba61fe2387a5c0bdae56c5f846831
andrija.mitrovic <andrijamitrovic11@gmail.com>
2022-03-26 15:45:55 +0100

    prebacivanje edita u jednu metodu

--- back/Igrannonica/Controllers/CsvController.cs
+++ back/Igrannonica/Controllers/CsvController.cs
@@ -57,69 +57,62 @@ namespace Igrannonica.Controllers
         }
 
         [HttpPost("updatefilecall")]
-        public IActionResult UpdateFileCall()
+        public async Task<IActionResult> UpdateFileCall()
         {
             using (var client = new HttpClient())
             {
                 var endpoint = new Uri("http://127.0.0.1:5000/editcell");
-                var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
-                return Ok(json);
-            }
-        }
-
-        [HttpPost("updateFile/{filename}")]
-        public async Task<IActionResult> UpdateFile()
-        {
-            var request = HttpContext.Request;// validation of Content-Type
-            // 1. first, it must be a form-data request
-            // 2. a boundary should be found in the Content-Type
-            if (!request.HasFormContentType ||
-                !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
-                string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
-            {
-                return Ok("los tip fajla");
-            }
-
-            var reader = new MultipartReader(mediaTypeHeader.Boundary.Value, request.Body);
-            var section = await reader.ReadNextSectionAsync();
-
-            // This sample try to get the first file from request and save it
-            // Make changes according to your needs in actual use
-            while (section != null)
-            {
-                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
-                    out var contentDisposition);
-
-                if (hasContentDispositionHeader && contentDisposition.DispositionType.Equals("form-data") &&
-                    !string.IsNullOrEmpty(contentDisposition.FileName.Value))
+                var request = HttpContext.Request;// validation of Content-Type
+                                                  // 1. first, it must be a form-data request
+                                                  // 2. a boundary should be found in the Content-Type
+                if (!request.HasFormContentType ||
+                    !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
+                    string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
                 {
-                    // Don't trust any file name, file extension, and file data from the request unless you trust them completely
-                    // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
-                    // In short, it is necessary to restrict and verify the upload
-                    // Here, we just use the temporary folder and a random file name
-
-                    // Get the temporary folder, and combine a random file name with it
-
-                    var trustedFileNameForDisplay = WebUtility.HtmlEncode(
-                            contentDisposition.FileName.Value);
-                    var folderName = Path.Combine("Resources", "CSVFilesUnauthorized");
-                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                    var RandomFileName = contentDisposition.FileName.Value;
-                    var fullPath = Path.Combine(pathToSave, RandomFileName);
-                    using (var targetStream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        await section.Body.CopyToAsync(targetStream);
-                        targetStream.Dispose();
-                    }
-                    return Ok(contentDisposition.FileName.Value);
+                    return Ok("los tip fajla");
                 }
 
-                section = await reader.ReadNextSectionAsync();
+                var reader = new MultipartReader(mediaTypeHeader.Boundary.Value, request.Body);
+                var section = await reader.ReadNextSectionAsync();
+
+                // This sample try to get the first file from request and save it
+                // Make changes according to your needs in actual use
+                while (section != null)
+                {
+                    var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+                        out var contentDisposition);
+
+                    if (hasContentDispositionHeader && contentDisposition.DispositionType.Equals("form-data") &&
+                        !string.IsNullOrEmpty(contentDisposition.FileName.Value))
+                    {
+                        // Don't trust any file name, file extension, and file data from the request unless you trust them completely
+                        // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
+                        // In short, it is necessary to restrict and verify the upload
+                        // Here, we just use the temporary folder and a random file name
+
+                        // Get the temporary folder, and combine a random file name with it
+
+                        var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                                contentDisposition.FileName.Value);
+                        var folderName = Path.Combine("Resources", "CSVFilesUnauthorized");
+                        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                        var RandomFileName = contentDisposition.FileName.Value;
+                        var fullPath = Path.Combine(pathToSave, RandomFileName);
+                        using (var targetStream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            await section.Body.CopyToAsync(targetStream);
+                            targetStream.Dispose();
+                        }
+                        return Ok(contentDisposition.FileName.Value);
+                    }
+
+                    section = await reader.ReadNextSectionAsync();
+                }
+                // If the code runs to this location, it means that no files have been saved
+                return BadRequest("No files data in the request.");
             }
-            // If the code runs to this location, it means that no files have been saved
-            return BadRequest("No files data in the request.");
         }
+
         
     }
 }
