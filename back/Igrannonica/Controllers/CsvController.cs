1c31f94f7e521b67ad50c1a2ed866d8cce97c070
andrija.mitrovic <andrijamitrovic11@gmail.com>
2022-03-26 15:18:47 +0100

    endpoint za prihvatanje editovanog fajla

--- back/Igrannonica/Controllers/CsvController.cs
+++ back/Igrannonica/Controllers/CsvController.cs
@@ -4,6 +4,9 @@ using Igrannonica.DataTransferObjects;
 using Igrannonica.Models;
 using CsvHelper;
 using System.Globalization;
+using Microsoft.Net.Http.Headers;
+using System.Net;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Igrannonica.Controllers
 {
@@ -19,54 +22,23 @@ namespace Igrannonica.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(CsvDTO csv)
+        public async Task<IActionResult> Edit(CsvEditRowDTO csv)
         {
 
             Models.File? file = _mySqlContext.File.Where(f => f.FileName == csv.fileName).FirstOrDefault();
             if (file == null)
                 return BadRequest("no file with that name");
-            var folderName = Path.Combine("Resources", "CSVFiles");
-            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-            var fileName = file.RandomFileName;
-            var fullPath = Path.Combine(pathToSave, fileName);
 
-            using var streamReader = new StreamReader(fullPath);
-            using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
-            var records =  csvReader.GetRecords<dynamic>();
-            var iterator = records.GetEnumerator();
-            for (int i = 0; i < csv.rowNumber; i++)
+            using (var client = new HttpClient())
             {
-                var moved = iterator.MoveNext();
-                if (moved == false)
-                {
-                    return BadRequest("zadat veci red od ukupno redova");
-                }
+                var endpoint = new Uri("http://127.0.0.1:5000/simpleget");
+                var result = client.GetAsync(endpoint).Result;
+                //var json = result.;
+                //return Ok(json);
+                return Ok(result);
             }
-            var record = iterator.Current;
-            var result = new RouteValueDictionary(record);
-            result[csv.columnName] = csv.value;
 
-
-            fileName = string.Format("{0}.csv", Path.GetRandomFileName().Replace(".", string.Empty));
-            fullPath = Path.Combine(pathToSave, fileName);
-            file.RandomFileName = fileName;
-            _mySqlContext.Update(file);
-            await _mySqlContext.SaveChangesAsync();
-            using (var targetStream = new StreamWriter(fullPath))
-            {
-                using (var csvWriter = new CsvWriter(targetStream, CultureInfo.InvariantCulture))
-                {
-                    await csvWriter.WriteRecordsAsync(records);
-                }
-                targetStream.Dispose();
-            }
-            return Ok(fileName);
-            //while(iterator.MoveNext())
-            //{
-            //    var record = iterator.Current;
-
-            //}
-            //return Ok(csv);
+            
         }
 
         [HttpGet("{filename}")]
@@ -83,5 +55,59 @@ namespace Igrannonica.Controllers
             var bytes = await System.IO.File.ReadAllBytesAsync(fullPath);
             return File(bytes, "csv/plain", Path.GetFileName(fullPath));
         }
+
+        [HttpPost("updateFile/{filename}")]
+        public async Task<IActionResult> UpdateFile()
+        {
+            var request = HttpContext.Request;// validation of Content-Type
+            // 1. first, it must be a form-data request
+            // 2. a boundary should be found in the Content-Type
+            if (!request.HasFormContentType ||
+                !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
+                string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
+            {
+                return Ok("los tip fajla");
+            }
+
+            var reader = new MultipartReader(mediaTypeHeader.Boundary.Value, request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            // This sample try to get the first file from request and save it
+            // Make changes according to your needs in actual use
+            while (section != null)
+            {
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+                    out var contentDisposition);
+
+                if (hasContentDispositionHeader && contentDisposition.DispositionType.Equals("form-data") &&
+                    !string.IsNullOrEmpty(contentDisposition.FileName.Value))
+                {
+                    // Don't trust any file name, file extension, and file data from the request unless you trust them completely
+                    // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
+                    // In short, it is necessary to restrict and verify the upload
+                    // Here, we just use the temporary folder and a random file name
+
+                    // Get the temporary folder, and combine a random file name with it
+
+                    var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                            contentDisposition.FileName.Value);
+                    var folderName = Path.Combine("Resources", "CSVFilesUnauthorized");
+                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                    var RandomFileName = string.Format("{0}.csv", Path.GetRandomFileName().Replace(".", string.Empty));
+                    var fullPath = Path.Combine(pathToSave, RandomFileName);
+                    using (var targetStream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        await section.Body.CopyToAsync(targetStream);
+                        targetStream.Dispose();
+                    }
+                    return Ok(contentDisposition.FileName.Value);
+                }
+
+                section = await reader.ReadNextSectionAsync();
+            }
+            // If the code runs to this location, it means that no files have been saved
+            return BadRequest("No files data in the request.");
+        }
+        
     }
 }
---
+++ back/Igrannonica/DataTransferObjects/CsvDeleteRowDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Igrannonica.DataTransferObjects
+{
+    public class CsvDeleteRowDTO
+    {
+        public string fileName { get; set; }
+        public List<int> rowNumber { get; set; }
+    }
+}
