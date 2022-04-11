16fc6b4590eba46644d910fff804126f2f4c5573
Milan Stosic <milanstosic00@gmail.com>
2022-04-11 17:53:02 +0200

    Izmena funkcije za upload autorizovanog korisnika

--- file storage/FileStorageServer.py
+++ file storage/FileStorageServer.py
@@ -17,30 +17,20 @@ def allowed_file(filename):
 
 @app.route('/uploadFileAuthorized', methods=['GET', 'POST'])
 def upload_file_authorized():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['AUTHORIZED_FOLDER'], filename))
-            return redirect(url_for('download_authorized_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-        <input type=file name=file>
-        <input type=submit value=Upload>
-    </form>
-    '''
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return {'message' : "No file part."}
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return {'message' : "No selected file."}
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['AUTHORIZED_FOLDER'], filename))
+        return {'message' : "Upload Successfull!"}
 
 @app.route('/uploadFileUnauthorized', methods=['GET', 'POST'])
 def upload_file_unauthorized():
