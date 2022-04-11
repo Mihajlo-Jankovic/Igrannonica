5354f5767c3c0b58cacb41a509cbfe9d73f189a3
andrija.mitrovic <andrijamitrovic11@gmail.com>
2022-04-11 22:14:57 +0200

    prebačen download fajlova na pajton server

--- back/Igrannonica/Controllers/CsvController.cs
+++ back/Igrannonica/Controllers/CsvController.cs
@@ -84,16 +84,14 @@ namespace Igrannonica.Controllers
         [HttpGet("{filename}")]
         public async Task<IActionResult> downloadfile(string filename)
         {
-            Models.File? file = _mySqlContext.File.Where(f => f.FileName == filename).FirstOrDefault();
+            Models.File? file = _mySqlContext.File.Where(f => f.RandomFileName == filename).FirstOrDefault();
             if (file == null)
                 return BadRequest("no file with that name");
-            var folderName = Path.Combine("Resources", "CSVFiles");
-            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-            var fileName = file.RandomFileName;
-            var fullPath = Path.Combine(pathToSave, fileName);
-
-            var bytes = await System.IO.File.ReadAllBytesAsync(fullPath);
-            return File(bytes, "csv/plain", Path.GetFileName(fullPath));
+            HttpClient client = new HttpClient();
+            var endpoint = new Uri("http://127.0.0.1:8000/downloadFile?" + "name=" + filename + "&authorization=authorized" );
+            var response = await client.GetAsync(endpoint);
+            var bytes = await response.Content.ReadAsByteArrayAsync();
+            return File(bytes, "csv/plain", filename);
         }
 
         [HttpPost("updatefilecall")]
--- file storage/FileStorageServer.py
+++ file storage/FileStorageServer.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from numpy import true_divide
 from werkzeug.utils import secure_filename
 import pandas as pd
 
@@ -15,8 +16,13 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/uploadFileAuthorized', methods=['GET', 'POST'])
-def upload_file_authorized():
+def isAuthorized(flag):
+    if flag==1:
+        return True
+    return False
+
+@app.route('/uploadFileAuthorized/<authorization>', methods=['GET', 'POST'])
+def upload_file_authorized(authorization):
     # check if the post request has the file part
     if 'file' not in request.files:
         flash('No file part')
@@ -29,33 +35,19 @@ def upload_file_authorized():
         return {'message' : "No selected file."}
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['AUTHORIZED_FOLDER'], filename))
+        if authorization == "authorized":
+            file.save(os.path.join(app.config['AUTHORIZED_FOLDER'], filename))
+        else:
+            file.save(os.path.join(app.config['UNAUTHORIZED_FOLDER'], filename))
         return {'message' : "Upload Successfull!"}
 
-@app.route('/uploadFileUnauthorized', methods=['GET', 'POST'])
-def upload_file_unauthorized():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return {'message' : "No file part."}
-    file = request.files['file']
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename.
-    if file.filename == '':
-        flash('No selected file')
-        return {'message' : "No selected file."}
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UNAUTHORIZED_FOLDER'], filename))
-        return {'message' : "Upload Successfull!"}
-
-@app.route('/downloadAuthorized/<name>')
-def download_authorized_file(name):
-    return send_from_directory(app.config["AUTHORIZED_FOLDER"], name)
-
-@app.route('/downloadUnauthorized/<name>')
-def download_unauthorized_file(name):
-    return send_from_directory(app.config["UNAUTHORIZED_FOLDER"], name)
+@app.route('/downloadFile', methods=['GET'])
+def download_file():
+    args = request.args
+    if(args['authorization'] == "authorized"):
+        return send_from_directory(app.config["AUTHORIZED_FOLDER"], args['name'])
+    else:
+        return send_from_directory(app.config["UNAUTHORIZED_FOLDER"], args['name'])
 
 @app.route("/")
 def hello():
