1ed06442bec17d09efd161afb09e56abb93ffeea
Mihajlo Jankovic <mihajlojankovic44@gmail.com>
2022-04-27 01:40:27 +0200

    Promenjen model eksperimenata

--- back/Igrannonica/DataTransferObjects/ExperimentDTO.cs
+++ back/Igrannonica/DataTransferObjects/ExperimentDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Igrannonica.DataTransferObjects
+﻿using Igrannonica.Models;
+
+namespace Igrannonica.DataTransferObjects
 {
     public class ExperimentDTO
     {
@@ -6,21 +8,8 @@
         public string name { get; set; }
         public string date { get; set; }
         public string fileName { get; set; }
-        public List<string> inputList { get; set; }
-        public string output { get; set; }
-        public string encodingType { get; set; }
-        public double ratio { get; set; }
-        public int numLayers { get; set; }
-        public List<int> layerList { get; set; }
-        public string activationFunction { get; set; }
-        public string regularization { get; set; }
-        public double regularizationRate { get; set; }
-        public string optimizer { get; set; }
-        public double learningRate { get; set; }
-        public string problemType { get; set; }
-        public string lossFunction { get; set; }
-        public List<string> metrics { get; set; }
-        public int numEpochs { get; set; }
-        public string results { get; set; }
+        public string realName { get; set; }
+        public string description { get; set; }
+        public List<Model> models { get; set; }
     }
 }
