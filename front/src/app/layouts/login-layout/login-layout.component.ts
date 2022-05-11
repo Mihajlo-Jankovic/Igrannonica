2b104d3a44d708741a2df402c7225c9d3d6c2714
Ana <apetrovicpmf@gmail.com>
2022-05-11 23:34:07 +0200

    Zaboravljena sifra (izgled divova)

--- front/src/app/layouts/login-layout/login-layout.component.ts
+++ front/src/app/layouts/login-layout/login-layout.component.ts
@@ -17,24 +17,51 @@ import { UserInfoService } from 'src/app/services/edit/user-info.service';
 export class LoginLayoutComponent implements OnInit {
 
   public loginForm: FormGroup;
+  public forgotPasswordIndicator: boolean;
+  public temporaryPasswordIndicator: boolean;
+  public newPasswordIndicator: boolean;
 
   constructor( private toastr: ToastrService,private formBuilder: FormBuilder, private loginService: LoginService, private cookie: CookieService, private router: Router) {
     this.loginForm = formBuilder.group({
       username: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$")]],
       password: ['', [Validators.required, Validators.pattern("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$")]]
     })
+
   }
 
   ngOnInit(): void {
+    this.forgotPasswordIndicator=false;
   }
 
   public get m() {
     return this.loginForm.controls;
   }
 
+  forgotPassword(){
+    this.forgotPasswordIndicator = true;
+    this.temporaryPasswordIndicator=false;
+    this.newPasswordIndicator=false;
+  }
+
+  createTemporaryPassword(){
+    this.forgotPasswordIndicator=false;
+    this.temporaryPasswordIndicator=true;
+    this.newPasswordIndicator=false;
+  }
+
+  backToLogin(){
+    this.forgotPasswordIndicator=false;
+    this.newPasswordIndicator=false;
+  }
+
+  createNewPassword(){
+    this.forgotPasswordIndicator = false;
+    this.temporaryPasswordIndicator=false;
+    this.newPasswordIndicator=true;
+  }
+
   save(username:string,password:string){
     sessionStorage.setItem('username',username);
-    sessionStorage.setItem('password', password);
   }
 
 
@@ -42,17 +69,26 @@ export class LoginLayoutComponent implements OnInit {
     if (form.value.username && form.value.password) {
       this.loginService.login(form.value.username, form.value.password).subscribe(token => {
         let JSONtoken: string = JSON.stringify(token);
-        let StringToken = JSON.parse(JSONtoken).token;
-        if (StringToken == "User not found"){
-          this.toastr.info('<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> <b>Wrong username</b>.', '', {
-            disableTimeOut: false,
-            closeButton: true,
-            enableHtml: true,
-            toastClass: "alert alert-info alert-with-icon",
-            positionClass: 'toast-top-center'
-          });
-        }
-        else if (StringToken == "Wrong password"){
+        let StringToken = JSON.parse(JSONtoken).token; 
+
+        this.save(form.value.username,form.value.password);
+        this.cookie.set("token", StringToken);
+        this.cookie.set("username",form.value.username);
+        this.toastr.info('<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> <b>Successful login</b>.', '', {
+          disableTimeOut: false,
+          closeButton: true,
+          enableHtml: true,
+          toastClass: "alert alert-info alert-with-icon",
+          positionClass: 'toast-top-center'
+        });
+        this.router.navigate(["upload"]);
+        
+        },err=>{
+          
+          let JSONtoken: string = JSON.stringify(err.error);
+          let StringToken = JSON.parse(JSONtoken).responseMessage;
+
+        if (StringToken == "Error: Wrong password!"){
           this.toastr.info('<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> <b>Wrong password</b>.', '', {
             disableTimeOut: false,
             closeButton: true,
@@ -61,18 +97,14 @@ export class LoginLayoutComponent implements OnInit {
             positionClass: 'toast-top-center'
           });
         }
-        else {
-          this.save(form.value.username,form.value.password);
-          this.cookie.set("token", StringToken);
-          this.cookie.set("username",form.value.username);
-          this.toastr.info('<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> <b>Successful login</b>.', '', {
+        else if(StringToken=="Error: Username not found!") {
+          this.toastr.info('<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> <b>Wrong username</b>.', '', {
             disableTimeOut: false,
             closeButton: true,
             enableHtml: true,
             toastClass: "alert alert-info alert-with-icon",
             positionClass: 'toast-top-center'
           });
-          this.router.navigate(['upload']);
         }
       })
     }
