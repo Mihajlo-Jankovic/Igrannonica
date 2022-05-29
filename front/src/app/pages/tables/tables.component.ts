f18666ac51d8955308b077b4643c8c99c483c62a
katarina.mosic <kata.99@hotmail.rs>
2022-05-29 02:21:59 +0200

    MissingValues&Outliers

--- front/src/app/pages/tables/tables.component.ts
+++ front/src/app/pages/tables/tables.component.ts
@@ -470,7 +470,7 @@ export class TablesComponent {
 
     if (!filter) {
       this.showStatisticDiv = true;
-      this.showStatistics(this.selectedColName);
+      this.showStatistics(this.selectedColName, true);
     }
     //*
     this.setInputOutput();
@@ -650,8 +650,8 @@ export class TablesComponent {
     this.rowLines = [];
   }
 
-  showStatistics(col: string) {
-    if (sessionStorage.getItem('statistics')) {
+  showStatistics(col: string, flag1: boolean) {
+    if (sessionStorage.getItem('statistics') && flag1 == false) {
       //this.statisticData = JSON.parse(sessionStorage.getItem('statistics'));
       //this.loadStatistics();
       this.statistic = JSON.parse(sessionStorage.getItem('statistics'));
@@ -918,7 +918,7 @@ export class TablesComponent {
 
     sessionStorage.removeItem('statistics');
     this.resetStatistic();
-    this.showStatistics(value);
+    this.showStatistics(value, false);
   }
 
   resetStatistic() {
@@ -1299,7 +1299,7 @@ export class TablesComponent {
       this.showTable(this.selectedType, this.selectedRow, this.page, false, this.selectedOutlierColumn);
       sessionStorage.removeItem('statistics');
       this.resetStatistic();
-      this.showStatistics(this.selectedColName);
+      this.showStatistics(this.selectedColName, true);
       this.selectedRows = [];
       this.poruka = "Delete successfull";
       //this.poruka = "Red/ovi su uspešno obrisani";
@@ -1339,7 +1339,7 @@ export class TablesComponent {
         this.reset()
         this.showTable(this.selectedType, this.selectedRow, this.page, false, this.selectedOutlierColumn);
         this.resetStatistic();
-        this.showStatistics(this.selectedColName);
+        this.showStatistics(this.selectedColName, true);
         this.poruka = "Edit successfull";
         // this.poruka = "Izmena je uspela";
         this.notify.showNotification(this.poruka);
