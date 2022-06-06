939350c7b9b8b96d5a802eadb3ba152a0195c7aa
Mihajlo Jankovic <mihajlojankovic44@gmail.com>
2022-06-06 02:21:06 +0200

    Izmena portova

--- front/src/app/configuration.ts
+++ front/src/app/configuration.ts
@@ -3,7 +3,7 @@ import { environment } from "src/environments/environment";
 
 export class Configuration
 {
-    public port : string = "https://localhost:7219"; // http://147.91.204.115:10106 https://localhost:7219
+    public port : string = "http://147.91.204.115:10106"; // http://147.91.204.115:10106 https://localhost:7219
     
 
     public login : string = this.port + "/api/User/login";
